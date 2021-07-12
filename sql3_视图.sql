62b986d7e549d1a6d4fcf26cce68a0ac762a2ca8
bowshape <30967873+851499540@users.noreply.github.com>
2021-07-12 22:54:25 +0100

    Update sql3_视图.sql

--- sql3_视图.sql
+++ sql3_视图.sql
@@ -12,8 +12,69 @@
 通过定义视图可以降低数据的冗余。
 
 
+一、视图
 
-
--- create
+-- 1. create
 CREATE VIEW <视图名称>(<列名1>,<列名2>,...) AS <SELECT语句>
+
+-- single table view
+CREATE VIEW productsum (product_type, cnt_product)
+AS
+SELECT product_type, COUNT(*)
+  FROM product
+ GROUP BY product_type ;
+ 
+ 
+ 
+-- two tables view
+CREATE VIEW view_shop_product(product_type, sale_price, shop_name)
+AS
+SELECT product_type, sale_price, shop_name
+  FROM product,                               --table1
+       shop_product                           --table2
+ WHERE product.product_id = shop_product.product_id;
+
+
+-- 2. change structure of the view
+ALTER VIEW <视图名> AS <SELECT语句>
+
+ALTER VIEW productSum
+    AS
+        SELECT product_type, sale_price
+          FROM Product
+         WHERE regist_date > '2009-09-11';
+         
+         
+         
+-- 3.change content of the view
+因为视图是一个虚拟表，所以对视图的操作就是对底层基础表的操作，所以在修改时只有满足底层基本表的定义才能成功修改。
+
+对于一个视图来说，如果包含以下结构的任意一种都是不可以被更新的：
+
+  聚合函数 SUM()、MIN()、MAX()、COUNT() 等。
+  DISTINCT 关键字。
+  GROUP BY 子句。
+  HAVING 子句。
+  UNION 或 UNION ALL 运算符。
+  FROM 子句中包含多个表。
+视图归根结底还是从表派生出来的，因此，如果原表可以更新，那么 视图中的数据也可以更新。反之亦然，如果视图发生了改变，而原表没有进行相应更新的话，就无法保证数据的一致性了。
+
+UPDATE productsum
+   SET sale_price = '5000'
+ WHERE product_type = '办公用品';
+ 
+但是并不推荐这种使用方式。而且我们在创建视图时也尽量使用限制不允许通过视图来修改表
+
+
+-- 4. delete
+DROP VIEW <视图名1> [ , <视图名2> …]
+
+DROP VIEW productSum;
+
+
+
+
+
+
+
 
